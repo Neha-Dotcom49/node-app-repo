5720757876a72772be03a141ef97569eee547b38
neha <sp028905@gmail.com>
2024-09-17 12:14:59 +0530

    Adding all files
    Adding all files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # node-app-repo
 This is Node App Repository
+# Project build steps
+1. clone the repository -<repo-url>
+2.npm install or npm update
+
+#Author
+Neha Patil
+Mobile: 7067451010
---
+++ index.js
@@ -0,0 +1,22 @@
+const os = require('os');
+console.log('My first app');
+console.log(`File Name: ${__filename}`);
+console.log(`Directory Name: ${__dirname}`);
+// console.log(global);
+// console.log(console);
+// console.log(process);
+// console.log(module);
+
+console.log("=============== Built in Module - OS =======================");
+console.log(`Arch: ${os.arch()}`);
+// console.log(`Cpus: ${os.cpus()}`);
+console.log(`Cpus:`,JSON.stringify(os.cpus(), null, 2));
+console.log(`Freemem: ${os.freemem()}`);
+console.log(`Totalmem: ${os.totalmem()}`);
+console.log(`Hostname: ${os.hostname()}`);
+console.log(`Plateform: ${os.platform()}`);
+console.log(`Release: ${os.release()}`);
+console.log(`Type: ${os.type()}`);
+console.log(`Uptime: ${os.uptime()}`);
+// console.log(`UserInfo: ${os.userInfo()}`);
+console.log(`UserInfo:`, JSON.stringify(os.userInfo(), null, 2));
